1a090e1248932a7453f92727f07d53521bbd0a9e
Carlos Araya <rayacarlo@Carloss-MacBook-Pro.local>
2020-10-06 22:12:20 -0300

    basic styling with css

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="Hero">
+      <div className="HeroGroup">
+      <SEO title="Home" />
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Protoype and build apps with React and Swift.</p>
+      <Link to="/page-2/">Watch the video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
