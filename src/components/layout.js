a2b14e218879996a7a08a52be79c4072fc921304
Carlos Araya <rayacarlo@Carloss-MacBook-Pro.local>
2020-10-07 12:26:57 -0300

    interactions and svg animations

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Header from './header'
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -24,7 +25,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
-        <main>{children}</main>
+      <Header />
+      <main>{children}</main>
     </>
   )
 }
